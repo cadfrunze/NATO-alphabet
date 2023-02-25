46ee9d6512f8fdc22348e4ba0e7ea7c811de23f0
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-02-25 14:07:18 +0200

    update

--- main.py
+++ main.py
@@ -8,19 +8,17 @@ data_words = pandas.read_csv('nato_phonetic_alphabet.csv')
 data_dict = {valoarea.letter: valoarea.code for (nimic, valoarea) in data_words.iterrows()}
 
 
-
-
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 def nato_alph(user_fun):
     user_fun = user_fun.replace(' ', '')
     user_list = [item for item in user_fun]
-    new_dict = [data_dict[letter] for letter in user_list if letter in data_dict.keys()]
+    new_dict = [{letter: data_dict[letter]} for letter in user_list if letter in data_dict.keys()]
     return new_dict
 
 
 while True:
     user_answer = input('Zii ceva: ').upper()
-    if user_answer == 'EXIT':
+    if user_answer == 'EXIT.':
         break
     print(nato_alph(user_fun=user_answer))
