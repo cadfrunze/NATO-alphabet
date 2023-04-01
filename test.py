a78a7a74b8f2722e704a6e8d25521ff90818c7bf
cadfrunze <marius1890@yahoo.com>
2023-04-01 21:58:31 +0300

    update!!!

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
 # with open('work_log.txt', 'a') as file:
 #     file.write('\n25.02')
-
-import pandas
-
-data = pandas.read_csv('nato_phonetic_alphabet.csv')
-
-
-for (index, row) in data.iterrows():
-    print(type(row))
+with open('work_log.txt', 'a') as file:
+    file.writelines('\n01.04')
